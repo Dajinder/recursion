3773f39e42ba228be9b195dbefa72b07fcc4272f
unknown <singh.dajinder13@gmail.com>
2020-05-07 23:11:23 +0530

    N-Queen and Knight Tour added

--- recursion.java
+++ recursion.java
@@ -1,41 +1,10 @@
 // package recursion_revision;
-
+import java.util.Arrays;
 public class recursion {
     public static void main(String[] args){
         solve();
     }
-    public static void solve(){
-        int[] arr = {2,3,5,7};
-        int tar = 10;
-        String ans = "";
-        int res = 0;
-        
-        // ans = coinchangeCombinations_INF(arr, 0, tar);
-        // ans = coinchangeCombinations(arr, 0, tar);
-        // res = coinchangePermutaion(arr, tar);
-        // System.out.println(coinchangePermutaion_INF(arr, tar, " "));
-        // System.out.println(coinChangeCombination_subseq(arr, 0, tar, ans));
- 
- 
- 
- 
-        // System.out.println(coinChangeCombination_subseq(arr,0,tar," "));
-        // System.out.println(coinChangeCombination_INF_subseq(arr, 0, tar," "));
-        // System.out.println(coinChangePermutation_INF_subseq(arr, 0, tar," "));
-        // System.out.println(coinChangePermutation_subseq(arr, 0, tar," "));
-
-        // int[] coins = {1,1,1,1,1};
-        // boolean[] vis = {false,false,false,false,false};
-        // System.out.println(queenCombination(vis, 0, 0, 3, " "));
-        // System.out.println(queenPermutation(vis, 0, 0, 3, ans));
-
-
-        boolean[][] boxes=new boolean[4][4];
-        int tnq = 4;
-
-        System.out.println(queensPermutation2D(boxes, tnq, " "));
-
-    }
+    
     public static int coinchangePermutaion_INF(int[] arr, int tar, String ans){
         if(tar==0){
             System.out.println(ans);
@@ -171,7 +140,36 @@ public class recursion {
     }
 
 
+    public static void coinChange(){
+        int[] arr = {2,3,5,7};
+        int tar = 10;
+        String ans = "";
+        int res = 0;
+        
+        // ans = coinchangeCombinations_INF(arr, 0, tar);
+        // ans = coinchangeCombinations(arr, 0, tar);
+        // res = coinchangePermutaion(arr, tar);
+        // System.out.println(coinchangePermutaion_INF(arr, tar, " "));
+        // System.out.println(coinChangeCombination_subseq(arr, 0, tar, ans));
+ 
+ 
+ 
+ 
+        // System.out.println(coinChangeCombination_subseq(arr,0,tar," "));
+        // System.out.println(coinChangeCombination_INF_subseq(arr, 0, tar," "));
+        // System.out.println(coinChangePermutation_INF_subseq(arr, 0, tar," "));
+        // System.out.println(coinChangePermutation_subseq(arr, 0, tar," "));
 
+        // int[] coins = {1,1,1,1,1};
+        // boolean[] vis = {false,false,false,false,false};
+        // System.out.println(queenCombination(vis, 0, 0, 3, " "));
+        // System.out.println(queenPermutation(vis, 0, 0, 3, ans));
+
+
+        // boolean[][] boxes=new boolean[4][4];
+        // int tnq = 4;
+
+    }
 
 
 
@@ -212,6 +210,21 @@ public class recursion {
         return count;
     }
 
+    public static int queensCombination2D(boolean[][] rooms, int room, int tnq, String ans){ // qpsf: queen place so far.
+        if (tnq==0){
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        for (int r = room; r < rooms.length*rooms[0].length; r++){
+            int x = r / rooms[0].length;
+            int y = r % rooms[0].length;
+            count += queensCombination2D(rooms, r + 1,tnq-1, ans + "(" + x + ", " + y + ") ");
+        }
+        return count;
+    }
+
     public static int queensPermutation2D(boolean[][] boxes,int tnq, String ans) // qpsf: queen place so far.
     {
         if (tnq==0)
@@ -234,4 +247,148 @@ public class recursion {
         }
         return count;
     }
+
+    // Nqueen
+
+
+    public static boolean isValid(boolean[][] rooms, int r, int c){
+
+        int[][] dir = {{0,-1},{-1,-1},{-1,0},{-1,1}};
+        for(int d=0;d<dir.length;d++){
+            for(int rad=1;rad<rooms.length;rad++){
+                int x = r+rad*dir[d][0];
+                int y = c+rad*dir[d][1];
+
+                if(x>=0 && y>=0 && x<rooms.length && y<rooms[0].length){
+                    if(rooms[x][y]) return false;
+                }
+                else break;
+            }
+        }
+        return true;
+
+    }
+    public static int Nqueen_01(boolean[][] rooms, int lpsf, int tnq, String ans){ // qpsf: queen place so far.
+        if (tnq==0){
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+
+        for (int r = lpsf; r < rooms.length*rooms[0].length; r++){
+            int x = r / rooms[0].length;
+            int y = r % rooms[0].length;
+            if(isValid(rooms, x, y)){
+                rooms[x][y] = true;
+                count += Nqueen_01(rooms, r + 1,tnq-1, ans + "(" + x + ", " + y + ") ");
+                rooms[x][y] = false;
+            }
+            
+        }
+        return count;
+    }
+
+    public static int Nqueen_02(boolean[][] board, int idx, int tnq, String ans) // qpsf: queen place so far.
+{
+    if (tnq==0 || idx==board.length*board[0].length)
+    {
+        if(tnq==0){
+           System.out.println(ans);
+           return 1;
+        }
+        return 0;
+    }
+
+    int count = 0;
+    
+    int x = idx / board[0].length;
+    int y = idx % board[0].length;
+    if(isValid(board,x,y)){
+        board[x][y]=true;
+        count += Nqueen_02(board, idx + 1,tnq-1, ans + "(" + x + ", " + y + ") ");
+        board[x][y]=false;
+    }
+    count += Nqueen_02(board, idx + 1,tnq, ans);
+    return count;
+}
+
+    public static boolean isValid_KT(boolean[][] board, int r, int c){
+        // int[][] dir = {{0,-1},{-1,-1},{-1,0},{-1,1}};
+        int[][] dirA={{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}};
+        int xMove[] = {2, 1, -1, -2, -2, -1, 1, 2}; 
+        int yMove[] = {1, 2, 2, 1, -1, -2, -2, -1}; 
+        for(int d=0;d<8;d++){
+            
+                int x = r+xMove[d];
+                int y = c+yMove[d];
+
+                if(x>=0 && y>=0 && x<board.length && y<board[0].length){
+                    if(board[x][y]) return false;
+                }
+                else break;
+            
+        }
+        return true;
+    }
+    
+     
+
+    public static boolean knightTour_01(int[][] board, int r,int c, int move){
+        board[r][c] = move;
+        if(move == 63){
+            for(int[] b : board){
+                for(int ele:b) System.out.print(ele+" ");
+                System.out.println();
+            }
+            return true;
+        }
+        boolean res = false;
+    
+        for(int d=0;d<8;d++){
+            
+            final int[] xMove = {2, 1, -1, -2, -2, -1, 1, 2}; 
+            final int[] yMove = {1, 2, 2, 1, -1, -2, -2, -1};
+
+            int x = r+xMove[d];
+            int y = c+yMove[d];
+            
+            if(x>=0 && y>=0 && x<8 && y<8 && board[x][y] == -1){
+                
+                res = res || knightTour_01(board, x, y, move+1);
+                
+            }
+            
+
+        }
+        board[r][c] = -1;
+        return res;
+        
+    }       
+
+    public static void knightTour(){
+        int [][] board = new int[8][8];
+        for(int i=0;i<board.length;i++){
+            Arrays.fill(board[i], -1);
+        }
+        System.out.println(knightTour_01(board, 0, 0, 0)); 
+    }
+
+
+    public static void Nqueen(){
+        boolean[][] boxes=new boolean[4][4];
+        int tnq = 4;
+        String ans = " ";
+        
+        System.out.println(Nqueen_02(boxes,0, tnq, ans));
+
+    }
+    
+    public static void solve(){
+        // coinChange();
+        // Nqueen();
+        knightTour();
+
+    }
+    
 }
